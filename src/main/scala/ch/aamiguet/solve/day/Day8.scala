015cf3f87e764769f79f29ccf7a26f9b0d26f958
Antoine Amiguet <amiguet.antoine@gmail.com>
2021-12-13 23:41:03 +0100

    Solving day 8, part 2. Probably not the best way to do it.

--- src/main/scala/ch/aamiguet/solve/day/Day8.scala
+++ src/main/scala/ch/aamiguet/solve/day/Day8.scala
@@ -8,8 +8,90 @@ case class DisplayWire(val codes: List[String], val values: List[String])
 object DisplayWire {
 
   def apply(line: String): DisplayWire = {
-    val sp = line.split("\\s\\|")
+    val sp = line.split("\\s\\|\\s")
     DisplayWire(sp(0).split("\\s+").toList, sp(1).split("\\s+").toList)
+  }
+
+}
+
+case class Display(
+  val top: String,
+  val topLeft: String,
+  val topRight: String,
+  val middle: String,
+  val bottomLeft: String,
+  val bottomRight: String,
+  val bottom: String,
+) {
+  val zero = (top + topLeft + topRight + bottomLeft + bottomRight + bottom).sorted
+  val one = (topRight + bottomRight).sorted
+  val two = (top + topRight + middle + bottomLeft + bottom).sorted
+  val three = (top + topRight + middle + bottomRight + bottom).sorted
+  val four = (topLeft + topRight + middle + bottomRight).sorted
+  val five = (top + topLeft + middle + bottomRight + bottom).sorted
+  val six = (top + topLeft + middle + bottomLeft + bottomRight + bottom).sorted
+  val seven = (top + topRight + bottomRight).sorted
+  val eight = (top + topLeft + topRight + middle + bottomLeft + bottomRight + bottom).sorted
+  val nine = (top + topLeft + topRight + middle + bottomRight + bottom).sorted
+
+  val code: Map[Int, String] = Map(
+    0 -> zero,
+    1 -> one,
+    2 -> two,
+    3 -> three,
+    4 -> four,
+    5 -> five,
+    6 -> six,
+    7 -> seven,
+    8 -> eight,
+    9 -> nine,
+  )
+
+  val reverseCode = code.map(e => e._2 -> e._1)
+
+}
+
+object Display {
+
+  def decode(code: String, dm: Map[Int, String]): Option[Int] =
+    code.length match {
+      case 2 =>
+        Some(1)
+      case 3 =>
+        Some(7)
+      case 4 =>
+        Some(4)
+      case 7 =>
+        Some(8)
+      case _ =>
+        None
+    }
+
+  def digitMap(codes: List[String], dm: Map[Int, String]): Map[Int, String] =
+    codes.foldLeft(dm) { (acc, c) =>
+      decode(c, dm) match {
+        case Some(digit) =>
+          acc + (digit -> c)
+        case _ =>
+          acc
+      }
+    }
+
+  def apply(codes: List[String]): Display = {
+    val dm = digitMap(codes, Map.empty[Int, String])
+
+    val top = dm.get(7).get diff dm.get(1).get
+    val d9 = dm.get(4).get + top
+    val bottom = codes.map(c => c diff d9).filter(_.length == 1).head
+    val nine = d9 + bottom
+    val d3 = dm.get(1).get + top + bottom
+    val middle = codes.map(c => c diff d3).filter(_.length == 1).head
+    val topLeft = dm.get(4).get diff dm.get(1).get diff middle
+    val bottomLeft = dm.get(8).get diff nine
+    val d5 = top + topLeft + middle + bottom
+    val bottomRight = codes.map(c => c diff d5).filter(_.length == 1).head
+    val topRight = dm.get(1).get diff bottomRight
+    Display(top, topLeft, topRight, middle, bottomLeft, bottomRight, bottom)
   }
 
 }
@@ -61,11 +143,19 @@ object Day8 extends Day {
   def easyDigitsCount(wires: List[DisplayWire]): Int = {
     val numbers = List(1, 4, 7, 8)
     wires.map { w =>
-      val dm = digitMap(w.codes, Map.empty[Int, String])
-      count(w.values, dm, numbers)
+      val display = Display.apply(w.codes)
+      numbers.foldLeft(0)((acc, n) =>
+        w.values.filter(v => v.sorted == display.code.get(n).get).length + acc
+      )
     }.sum
   }
 
+  def allDigitSum(wires: List[DisplayWire]): Int =
+    wires.map { w =>
+      val display = Display.apply(w.codes)
+      w.values.map(v => display.reverseCode.get(v.sorted).get.toString).mkString("").toInt
+    }.sum
+
   def part1 = println(s"Count of easy numbers is ${easyDigitsCount(wires)}")
-  def part2 = ???
+  def part2 = println(s"Sum of all display ${allDigitSum(wires)}")
 }
--- src/test/scala/ch/aamiguet/solve/day/Day8Spec.scala
+++ src/test/scala/ch/aamiguet/solve/day/Day8Spec.scala
@@ -7,20 +7,22 @@ class Day8Spec extends Specification {
   "Day 8 specification".br
 
   val single = DisplayWire(
-    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |cdfeb fcadb cdfeb cdbaf"
+    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
   )
 
+  val singleDisplay = Display("d", "e", "a", "f", "g", "b", "c")
+
   val dw = List(
-    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |fdgacbe cefdb cefbgd gcbe",
-    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |fcgedb cgb dgebacf gc",
-    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |cg cg fdcagb cbg",
-    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |efabcd cedba gadfec cb",
-    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |gecf egdcabf bgf bfgea",
-    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |gebdcfa ecba ca fadegcb",
-    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |cefg dcbef fcge gbcadfe",
-    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |ed bcgafe cdgba cbgef",
-    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |gbdfcae bgc cg cgb",
-    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |fgae cfgab fg bagce",
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
   ).map(DisplayWire.apply)
 
   "Raw decode" >> {
@@ -67,6 +69,14 @@ class Day8Spec extends Specification {
 
   "Count of easy numbers" >> {
     Day8.easyDigitsCount(dw) mustEqual 26
+  }
+
+  "Display decoding" >> {
+    Display(single.codes) mustEqual singleDisplay
+  }
+
+  "Sum of all digits" >> {
+    Day8.allDigitSum(dw) mustEqual 61229
   }
 
 }
